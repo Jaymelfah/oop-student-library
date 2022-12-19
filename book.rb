1ae4a68caf323c07256ea5a9c840a69fc7f9c654
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2022-12-19 20:40:53 +0000

    fix linters

--- book.rb
+++ book.rb
@@ -1,17 +1,16 @@
 require_relative './rental'
 
-class Book 
-    attr_accessor :title, :author
-    attr_reader :rentals 
+class Book
+  attr_accessor :title, :author
+  attr_reader :rentals
 
-    def initialize(title, author)
-        @title = title
-        @author = author
-        @rentals = []
-    end
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
 
-    def rent_book(date, person)
-        Rental.new(date, person, self)
-    end
-
+  def rent_book(date, person)
+    Rental.new(date, person, self)
+  end
 end
