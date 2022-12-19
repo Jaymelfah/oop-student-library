fbccf172c5432ba65aacf6dad8e4be0815d581a9
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2022-12-19 20:39:30 +0000

    create associations

---
+++ book.rb
@@ -0,0 +1,17 @@
+require_relative './rental'
+
+class Book 
+    attr_accessor :title, :author
+    attr_reader :rentals 
+
+    def initialize(title, author)
+        @title = title
+        @author = author
+        @rentals = []
+    end
+
+    def rent_book(date, person)
+        Rental.new(date, person, self)
+    end
+
+end
