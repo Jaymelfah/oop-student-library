66a6998ea0e642702d273f687269b96acba91d7b
BELINDA <kennyegun240@gmail.com>
2023-01-09 15:17:58 +0100

    Remove listings methods

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require_relative './rental'
 require_relative './book'
 require_relative './show_menu'
 require_relative './creation'
+require_relative 'listings'
 
 class App
   include ShowMenu
@@ -66,31 +67,52 @@ class App
   #   end
   # end
 
-  def list_books
-    if @book.empty?
-      puts 'No books available'
-      show_menu
-    end
-    puts 'List of available books:'
-    puts ''
-    @book.each_with_index do |item, i|
-      puts "#{i} Title: #{item.title}, Author: #{item.author}"
-    end
-    show_menu
-  end
+#   class Listings
+#   include ShowMenu
 
-  def list_people
-    if @people.empty?
-      puts 'No people found today'
-      show_menu
-    end
-    puts 'List of people:'
-    puts ''
-    @people.each_with_index do |item, i|
-      puts "#{i}-- [#{item.class}] Name:#{item.name},  Age: #{item.age}"
-    end
-    show_menu
-  end
+#   def initialize(people = [], book = [], rental = [])
+#     @book = book
+#     @people = people
+#     @rental = rental
+#   end
+
+#   def list_books
+#     if @book.empty?
+#       puts 'No books available'
+#       show_menu
+#     end
+#     puts 'List of available books:'
+#     puts ''
+#     @book.each_with_index do |item, i|
+#       puts "#{i} Title: #{item.title}, Author: #{item.author}"
+#     end
+#     show_menu
+#   end
+
+#   def list_people
+#     if @people.empty?
+#       puts 'No people found today'
+#       show_menu
+#     end
+#     puts 'List of people:'
+#     puts ''
+#     @people.each_with_index do |item, i|
+#       puts "#{i}-- [#{item.class}] Name:#{item.name},  Age: #{item.age}"
+#     end
+#     show_menu
+#   end
+
+#   def list_rentals
+#     puts 'Select ID of any person (Please type the number of the ID)'
+#     @people.each { |item| puts "id: #{item.id}, Person: #{item.name}" }
+#     print 'Person id: '
+#     id = gets.chomp.to_i
+#     @rental.each do |item|
+#       puts "Date: #{item.date}, Book: #{item.book.title} by #{item.book.author}" if item.person.id.to_i == id
+#     end
+#     show_menu
+#   end
+# end
 
   # class Creation
   #   include ShowMenu
@@ -171,17 +193,6 @@ class App
   #     show_menu
   #   end
   # end
-
-  def list_rentals
-    puts 'Select ID of any person (Please type the number of the ID)'
-    @people.each { |item| puts "id: #{item.id}, Person: #{item.name}" }
-    print 'Person id: '
-    id = gets.chomp.to_i
-    @rental.each do |item|
-      puts "Date: #{item.date}, Book: #{item.book.title} by #{item.book.author}" if item.person.id.to_i == id
-    end
-    show_menu
-  end
 
   # def show_menu
   #   puts ''
