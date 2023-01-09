a3a0e33486eef4efcbc9f021442b5e5eb354941b
BELINDA <kennyegun240@gmail.com>
2023-01-09 15:20:03 +0100

    Include listings in the options method

--- options.rb
+++ options.rb
@@ -1,19 +1,22 @@
 module Options
   def options(input)
     creation = Creation.new(@people, @book, @rental)
+    listings = Listings.new(@people, @book, @rental)
     case input
     when '1'
-      list_books
+      listings.list_people
+
     when '2'
-      list_people
+      listings.list_books
+
     when '3'
       @people.push(creation.create_person)
     when '4'
       @book.push(creation.create_book)
     when '5'
-      @rentals.push(creation.create_rental)
+      @rental.push(creation.create_rental)
     when '6'
-      list_rentals
+      listings.list_rentals
     else
       'Enter a number between from 1 to 7'
     end
