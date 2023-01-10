784aecc366e8928cd06a3b81c6057bf04fd8d5e5
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2023-01-10 09:40:22 +0000

    create methods for fetching and updating data in storage files

--- show_menu.rb
+++ show_menu.rb
@@ -9,4 +9,28 @@ module ShowMenu
     puts ''
     run
   end
+
+  def get_data(file)
+    path_file = "./data/#{file}.json"
+    File.new(path_file, 'w+') unless File.exist?(path_file)
+    File.write(path_file, '[]') if File.empty?(path_file)
+    file_contents = File.read(path_file)
+    JSON.parse(file_contents)
+  end
+
+  def update_data(file, data)
+    opts = {
+      array_nl: "\n",
+      object_nl: "\n",
+      indent: '  ',
+      space_before: ' ',
+      space: ' '
+    }
+    json = JSON.generate(data, opts)
+    File.write("./data/#{file}.json", json)
+  end
+
+  def show_books
+    get_data('books').map { |item| }
+  end
 end
